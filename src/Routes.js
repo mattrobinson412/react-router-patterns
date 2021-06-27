31e4254b4d1efe8f7dd6a2910bfaa341fcf83937
Matt Allen Robinson <mattrobinson412@gmail.com>
2021-06-27 18:03:19 -0400

    Add files via upload

--- src/Routes.js
+++ src/Routes.js
@@ -1,23 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { Switch, Route, Redirect, BrowserRouter } from "react-router-dom";
 
-import { Route, BrowserRouter, Redirect, Switch } from "react-router-dom";
+import Colors from "./Colors";
+import NewColorForm from "./NewColorForm";
+import Color from "./Color";
 
-import Dogs from "./Dogs";
-import FilterDogs from "./FilterDogs";
 
-// Component for Routes in Dog app. //
-const Routes = ({dogs}) => {
-    return (
-        <Switch>
-            <Route exact path="/dogs/:name">
-                <FilterDogs dogs={dogs} />
-            </Route>
-            <Route exact path="/dogs">
-                <Dogs dogs={dogs} />
-            </Route>
-            <Redirect to="/dogs" />
-        </Switch>
-      );
+function Routes() {
+
+  const initialColors = JSON.parse(localStorage.getItem("colors")) || {
+    red: "#FF0000",
+    green: "#00FF00",
+    blue: "#0000FF"
+  };
+  const [colors, updateColors] = useState(initialColors);
+
+  useEffect(
+    () => localStorage.setItem("colors", JSON.stringify(colors)),
+    [colors]
+  );
+
+  function handleAdd(newColorObj) {
+    updateColors(prevColors => ({ ...prevColors, ...newColorObj }));
+  }
+
+  function renderCurrentColor(props) {
+    const { color } = props.match.params;
+    const hex = colors[color];
+    return <Color {...props} hex={hex} color={color} />;
+  };
+
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/colors">
+          <Colors colors={colors} />
+        </Route>
+        <Route exact path="/colors/new">
+          <NewColorForm addColor={handleAdd} />
+        </Route>
+        <Route path="/colors/:color" render={renderCurrentColor} />
+        <Redirect to="/colors" />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default Routes;
